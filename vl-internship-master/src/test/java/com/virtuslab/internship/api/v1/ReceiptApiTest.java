6cff016d72d0cd0306800312d4dc7a76b8363bfe
bbronek <barbro214@gmail.com>
2022-03-26 17:52:07 +0100

    Update receipt api tests

--- vl-internship-master/src/test/java/com/virtuslab/internship/api/v1/ReceiptApiTest.java
+++ vl-internship-master/src/test/java/com/virtuslab/internship/api/v1/ReceiptApiTest.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.virtuslab.internship.basket.Basket;
 import com.virtuslab.internship.product.ProductDb;
+import com.virtuslab.internship.receipt.Receipt;
+import com.virtuslab.internship.receipt.ReceiptGenerator;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +14,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import java.lang.reflect.Type;
 
 import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ReceiptApiTest {
@@ -20,14 +23,16 @@ class ReceiptApiTest {
 
     @Test
     void shouldReturn201StatusCodeAfterSendingProperBasketJson() {
-        String basketJson = generateJSONBasket();
+        Basket cart = generateBasket();
+        Gson gson = new Gson();
+        Type gsonType = new TypeToken<Basket>(){}.getType();
 
         given().
             port(randomServerPort).
                 header("Content-Type", "application/json").
                 contentType(ContentType.JSON).
                 accept(ContentType.JSON).
-                body(basketJson).
+                body(gson.toJson(cart, gsonType)).
             when().
                 post("/api/v1/basket").
             then().
@@ -52,21 +57,49 @@ class ReceiptApiTest {
 
     @Test
     void shouldReturn405StatusCodeIfClientUsedGetRequest() {
-        String basketJson = generateJSONBasket();
+        Basket cart = generateBasket();
+        Gson gson = new Gson();
+        Type gsonType = new TypeToken<Basket>(){}.getType();
 
         given().
-                port(randomServerPort).
+            port(randomServerPort).
                 header("Content-Type", "application/json").
                 contentType(ContentType.JSON).
                 accept(ContentType.JSON).
-                body(basketJson).
-                when().
+                body(gson.toJson(cart, gsonType)).
+            when().
                 get("/api/v1/basket").
-                then().
+            then().
                 statusCode(405);
     }
 
-    String generateJSONBasket() {
+    @Test
+    void shouldReturnProperJsonData() {
+        var cart = generateBasket();
+        var receiptGenerator = new ReceiptGenerator();
+        var receipt = receiptGenerator.generate(cart);
+
+        Gson gson = new Gson();
+        Type gsonCartType = new TypeToken<Basket>(){}.getType();
+        Type gsonReceiptType = new TypeToken<Receipt>(){}.getType();
+
+        String responseJSON = given().
+                        port(randomServerPort).
+                            header("Content-Type", "application/json").
+                            contentType(ContentType.JSON).
+                            accept(ContentType.JSON).
+                            body(gson.toJson(cart, gsonCartType)).
+                        when().
+                            post("/api/v1/basket").
+                        then().
+                            statusCode(201).
+                        extract().
+                        asString();
+
+        assertEquals(responseJSON, gson.toJson(receipt, gsonReceiptType));
+    }
+
+    Basket generateBasket() {
         var productDb = new ProductDb();
         var cart = new Basket();
         var milk = productDb.getProduct("Milk");
@@ -78,9 +111,6 @@ class ReceiptApiTest {
         cart.addProduct(bread);
         cart.addProduct(apple);
 
-        Gson gson = new Gson();
-        Type gsonType = new TypeToken<Basket>(){}.getType();
-
-        return gson.toJson(cart, gsonType);
+        return cart;
     }
 }
